c5418f20d3a38291ef9fae3ad5398d32b6d97282
jmartin111 <chess.logic@gmail.com>
2021-11-04 10:43:23 -0700

    Initial commit

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qcah2%vhal0hau5t_cqc$g00n$l5tji%3b04zxtqryl2my73p1'
+SECRET_KEY = ' '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
